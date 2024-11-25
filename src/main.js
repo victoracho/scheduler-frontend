2822c1bb9dfe0e468200088d1370dcdc4b50068d
johan.noy <johan.noy96@gmail.com>
2024-11-25 14:47:01 -0500

    add datepicker lib

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import { pinia } from './store/scheduler';
 import App from './App.vue'
 const app = createApp(App)
 
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+
+// Use plugin with optional defaults
+app.use(VCalendar, {})
+
 app.use(pinia)
 app.provide('user', user)
 app.provide('deal', deal)
